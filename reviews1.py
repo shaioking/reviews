56fc0629ec63075a650d3f4dd03899c00fa76ef0
YUNG <shaioking@gmail.com>
2021-01-26 14:19:11 +0800

    add word count

---
+++ reviews1.py
@@ -0,0 +1,64 @@
+data = []
+count = 0
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+		count += 1 #count = count + 1 
+		if count % 1000 == 0:
+		    print(len(data))
+print('檔案讀取完了,總共有', len(data), '筆資料')
+
+
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print('留言平均是', sum_len/len(data))
+
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言的長度小於100')
+print(new[0])
+print(new[1])
+
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆留言提到Good')
+
+#文字計數
+wc = {} #word_count
+for d in data:
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1
+
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])
+
+
+print(len(wc))
+print(wc['Shane'])
+
+while True:
+	word = input('你要查什麼字呢?:')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word, '出現過的次數為', wc[word])
+	else:
+		print('這個字沒有出現過')
+print('感謝使用')
+# print(wc)	
+
+# 	 break#
+# key value
